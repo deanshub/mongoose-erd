6c801d9169f40b7cd5f901987ed457b8b0ebd5ef
Dean Shub <deanshub@gmail.com>
2020-09-09 20:50:35 +0300

    Nissix "prettier" plugin

--- example/post.js
+++ example/post.js
@@ -1,28 +1,32 @@
-import mongoose from 'mongoose'
+import mongoose from "mongoose";
 
 const schema = mongoose.Schema({
   player: {
     type: String,
-    ref: 'Player',
+    ref: "Player",
     required: true,
   },
   content: {
     type: mongoose.Schema.Types.Mixed,
     required: true,
   },
-  photos: [{
-    type: String,
-    required: true,
-    default: [],
-  }],
-  likes: [{
-    type: String,
-    ref: 'Player',
-    required: true,
-    default: [],
-  }],
+  photos: [
+    {
+      type: String,
+      required: true,
+      default: [],
+    },
+  ],
+  likes: [
+    {
+      type: String,
+      ref: "Player",
+      required: true,
+      default: [],
+    },
+  ],
   updated: { type: Date, default: Date.now },
   created: { type: Date, default: Date.now },
-})
+});
 
-const Post = mongoose.model('Post', schema)
+const Post = mongoose.model("Post", schema);
--- index.js
+++ index.js
@@ -1,121 +1,127 @@
-import fs from 'fs'
-import os from 'os'
-let mongoose
+import fs from "fs";
+import os from "os";
+let mongoose;
 
-let relations={}
+let relations = {};
 
-const getSchemaTitle = (schemaName)=>{
-  return `title {label: "${schemaName}", size: "20"}${os.EOL}${os.EOL}`
-}
-const getModelTitle = (modelName)=>{
-  return `[${modelName}] {bgcolor: "#d0e0d0"}${os.EOL}`
-}
-const getTypeString = (type)=>{
-  if (Array.isArray(type)){
-    if (!type[0]){
-      return 'Undefined'
-    }else{
-      return getTypeString(type[0])
+const getSchemaTitle = (schemaName) => {
+  return `title {label: "${schemaName}", size: "20"}${os.EOL}${os.EOL}`;
+};
+const getModelTitle = (modelName) => {
+  return `[${modelName}] {bgcolor: "#d0e0d0"}${os.EOL}`;
+};
+const getTypeString = (type) => {
+  if (Array.isArray(type)) {
+    if (!type[0]) {
+      return "Undefined";
+    } else {
+      return getTypeString(type[0]);
     }
   }
 
-  switch (type){
+  switch (type) {
     case mongoose.Schema.Types.ObjectId:
-      return 'ObjectId'
+      return "ObjectId";
     case String:
-      return 'String'
+      return "String";
     case Date:
-      return 'Date'
+      return "Date";
     case Number:
-      return 'Number'
+      return "Number";
     case Boolean:
-      return 'Boolean'
+      return "Boolean";
     case Buffer:
-      return 'Buffer'
+      return "Buffer";
     case mongoose.Schema.Types.Mixed:
-      return 'Object'
+      return "Object";
     default:
-      return 'Unknown'
+      return "Unknown";
   }
-}
-const getAttributeText = (modelName, model, attr)=>{
-  let attrModel = model[attr]
-  if (!attrModel){
-    attrModel={}
+};
+const getAttributeText = (modelName, model, attr) => {
+  let attrModel = model[attr];
+  if (!attrModel) {
+    attrModel = {};
   }
-  const {alias,required,unique,index, ref} = attrModel
+  const { alias, required, unique, index, ref } = attrModel;
 
-  const pkText = attr==='_id'?'*':''
-  const uniqueText = unique?'+':''
+  const pkText = attr === "_id" ? "*" : "";
+  const uniqueText = unique ? "+" : "";
 
-  const aliasText = alias?` (${alias})`:''
-  const defaultText = attrModel.default?`, [${attrModel.default}]`:''
-  const indexText = index?', Index':''
-  const mainText = `${attr} {label: "${attr} ${aliasText}${required?', not null':''}${defaultText}${indexText}"}`
+  const aliasText = alias ? ` (${alias})` : "";
+  const defaultText = attrModel.default ? `, [${attrModel.default}]` : "";
+  const indexText = index ? ", Index" : "";
+  const mainText = `${attr} {label: "${attr} ${aliasText}${
+    required ? ", not null" : ""
+  }${defaultText}${indexText}"}`;
 
-  if (ref){
-    if (!relations[modelName]){
-      relations[modelName] = []
+  if (ref) {
+    if (!relations[modelName]) {
+      relations[modelName] = [];
     }
 
-    if (!relations[modelName].includes(ref)){
-      relations[modelName].push(ref)
+    if (!relations[modelName].includes(ref)) {
+      relations[modelName].push(ref);
     }
   }
 
-  return `${pkText}${uniqueText}${mainText}${os.EOL}`
-}
+  return `${pkText}${uniqueText}${mainText}${os.EOL}`;
+};
 
-const getRelationText = (table1, table2)=>{
-  return `${table1} *--* ${table2} {label: "${table1}"}${os.EOL}`
-}
+const getRelationText = (table1, table2) => {
+  return `${table1} *--* ${table2} {label: "${table1}"}${os.EOL}`;
+};
 
-const toString = (localMongoose)=>{
-  mongoose = localMongoose
-  const modelNames = mongoose.modelNames()
-  if (!mongoose.models[modelNames[0]]){
-    throw new Error('mongoose models not loaded, please load them before using mongoose-erd tool')
+const toString = (localMongoose) => {
+  mongoose = localMongoose;
+  const modelNames = mongoose.modelNames();
+  if (!mongoose.models[modelNames[0]]) {
+    throw new Error(
+      "mongoose models not loaded, please load them before using mongoose-erd tool"
+    );
   }
 
-  relations = {}
-  const dbName = mongoose.models[modelNames[0]].db.name
-  let schemaString = getSchemaTitle(dbName)
+  relations = {};
+  const dbName = mongoose.models[modelNames[0]].db.name;
+  let schemaString = getSchemaTitle(dbName);
 
-  schemaString+=`# Entities${os.EOL}`
-  modelNames.forEach((modelName)=>{
-    const model = mongoose.models[modelName].schema.obj
-    const modelTitle = getModelTitle(modelName)
-    schemaString+=modelTitle
-    const idAttributeText = getAttributeText(modelName, model, '_id')
-    schemaString+=idAttributeText
-    Object.keys(model).filter(col=>col!=='_id').forEach((attrName)=>{
-      const attributeText = getAttributeText(modelName, model, attrName)
-      schemaString+=attributeText
-    })
-    schemaString+=os.EOL
-  })
+  schemaString += `# Entities${os.EOL}`;
+  modelNames.forEach((modelName) => {
+    const model = mongoose.models[modelName].schema.obj;
+    const modelTitle = getModelTitle(modelName);
+    schemaString += modelTitle;
+    const idAttributeText = getAttributeText(modelName, model, "_id");
+    schemaString += idAttributeText;
+    Object.keys(model)
+      .filter((col) => col !== "_id")
+      .forEach((attrName) => {
+        const attributeText = getAttributeText(modelName, model, attrName);
+        schemaString += attributeText;
+      });
+    schemaString += os.EOL;
+  });
 
-  schemaString+=`# Relationships${os.EOL}`
-  Object.keys(relations).forEach(table1=>{
-    relations[table1].forEach(table2=>{
-      const relationText = getRelationText(table1, table2)
-      schemaString+=relationText
-    })
-  })
+  schemaString += `# Relationships${os.EOL}`;
+  Object.keys(relations).forEach((table1) => {
+    relations[table1].forEach((table2) => {
+      const relationText = getRelationText(table1, table2);
+      schemaString += relationText;
+    });
+  });
 
-  return schemaString
-}
+  return schemaString;
+};
 export default {
   toString,
-  toFile(localMongoose, filePath='schema.er'){
-    return new Promise((resolve,reject)=>{
-      fs.writeFile(filePath, toString(localMongoose),(err)=>{
-        if (err){
-          reject(err)
-        }else{
-          resolve(filePath)
+  toFile(localMongoose, filePath = "schema.er") {
+    return new Promise((resolve, reject) => {
+      fs.writeFile(filePath, toString(localMongoose), (err) => {
+        if (err) {
+          reject(err);
+        } else {
+          resolve(filePath);
         }
-      })
-    })
-  }
-}
+      });
+    });
+  },
+};
